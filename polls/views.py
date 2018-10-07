e8af80c2e2433969ad7ecaa7308d795dbb81ffd2
Ryan <rkilbride13@gmail.com>
2018-10-07 16:51:07 -0700

    Stabilized some things. Ready for part 5 or part 6

--- polls/views.py
+++ polls/views.py
@@ -15,11 +15,11 @@ from .models import Choice, Question
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
+    context_object_name = 'arrLatestQuestions'
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.order_by('-dtmPublishDate')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
